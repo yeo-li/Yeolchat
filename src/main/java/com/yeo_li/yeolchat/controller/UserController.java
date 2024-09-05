649223ec1c19a0ef1d85ad632a65d30dea376577
parkseongyeol <ert2154@naver.com>
2024-09-05 19:20:38 +0900

    UserController : 제작 완료
    POST localhost/users : 회원가입 저장

--- src/main/java/com/yeo_li/yeolchat/controller/UserController.java
+++ src/main/java/com/yeo_li/yeolchat/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.yeo_li.yeolchat.controller;
 
 import com.yeo_li.yeolchat.dto.user.signUp.UserSignUpRequest;
+import com.yeo_li.yeolchat.entity.User;
 import com.yeo_li.yeolchat.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,13 +15,13 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/save")
-    public void createUser(@RequestBody UserSignUpRequest userDTO){
+    @PostMapping("")
+    public ResponseEntity<String> createUser(@RequestBody UserSignUpRequest userDTO){
 
         userService.signUp(userDTO);
-        //User user1 = userService.findByUserId("ert215465");
 
+
+        HttpHeaders header = new HttpHeaders();
+        return new ResponseEntity<>("회원가입이 정상적으로 수행되었습니다.", HttpStatus.OK);
     }
 }
-
-
